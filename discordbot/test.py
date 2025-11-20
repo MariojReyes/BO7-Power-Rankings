b92a0db3b02c804d52e4517ba93d2613867bb130
MarioJReyes <Mariojreyes2003@gmail.com>
2025-11-20 02:16:10 -0600

    testing inserts to supabase

--- discordbot/test.py
+++ discordbot/test.py
@@ -1,6 +1,8 @@
 from supabase import create_client
 from dotenv import load_dotenv
 import os
+#print("WORKING DIR:", os.getcwd())
+
 
 load_dotenv()
 
@@ -8,3 +10,14 @@ url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_SERVICE_KEY")      
 
 supabase = create_client(url, key)
+
+# test insert
+result = supabase.table("matches").insert({
+    "by_who": "Mario",
+    "map_id": 1,
+    "mode_id": 1,
+    "guild_score": 250,
+    "jsoc_score": 240
+}).execute()
+
+print(result)
